c515ff15342f555567c98b92f884bebd5820ca9f
Ganesh Kumar U <94672507+ganeshkumar28@users.noreply.github.com>
2024-02-03 15:54:40 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require('path');
+const { Server } = require("socket.io");
+const ACTIONS = require("./src/Actions");
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+const userSocketMap = {};
+const getAllConnectedClients = (roomId) => {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
+};
+
+io.on("connection", (socket) => {
+  // console.log('Socket connected', socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    // notify that new user join
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit("joined", {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+
+  // sync the code
+  socket.on('code-change', ({ roomId, code }) => {
+    socket.in(roomId).emit('code-change', { code });
+  });
+  // when new user join the room all the code which are there are also shows on that persons editor
+  socket.on('sync-code', ({ socketId, code }) => {
+    io.to(socketId).emit('code-change', { code });
+  });
+
+  // leave room
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    // leave all the room
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit("disconnected", {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is runnint on port ${PORT}`));
